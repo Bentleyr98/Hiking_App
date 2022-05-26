0eb735a3af38f6c00c5291caecbbf67146b1f4fc
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-05-25 20:57:36 -0600

    Filled in README document and finished adding points for hikes.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Hiking_App
-A simple app that shows some of the local hikes in the Rexburg/Idaho Falls ID area
+# Overview
+This software uses GIS mapping to show 20 hikes in the Idaho Falls area. It's designed for the user to simply click on the diamond designating the hike and a popup
+window will show the elevation gain and length of the hike. This will allow users to see where hikes are at near them and how long the hike is.
+
+Before this project, I had never touched GIS mapping or Kotlin before. The more I learned about it, the more intrigued I was by this software and I really
+wanted to figure it out and do a project with it. Figuring out the software took some effort as some of the recent changes to arcgis hasn't been 
+documented yet, but in completing the project, it really helped me grow as a software engineer. I hope this project will help you learn a bit more
+as well about GIS mapping from this software.
+
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+IDE
+* Android Studio 
+
+Language
+* Kotlin
+
+Libraries
+* ArcGIS
+* Android
+* Java Util
+* Kotlin Math
+
+
+# Useful Websites
+
+* [ArcGIS Runtime API for Android](https://developers.arcgis.com/android/)
+* [Stack Overflow](https://stackoverflow.com/)
+
+# Future Work
+
+* Make popup boxes disappear when user clicks outside of the points
+* Add a welcome screen
+* Add filters
+* Add an option and place for user to save hikes to view later
+* Add images for hikes
+* Add difficulty level for hikes and color code accordingly
--- app/src/main/java/com/example/hikes/MainActivity.kt
+++ app/src/main/java/com/example/hikes/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
 
         // set the viewpoint, Viewpoint(latitude, longitude, scale)
         mapView.setViewpoint(Viewpoint(43.825386, -111.792824
-            , 1190000.0))
+            , 2000000.0))
 
     }
 
@@ -67,6 +67,24 @@ class MainActivity : AppCompatActivity() {
     private fun createHikeGraphics(): Array<Graphic> {
         val hike1loc = Point(-111.9902778, 43.78555556, SpatialReferences.getWgs84())
         val hike2loc = Point(-111.7178538, 43.65749903, SpatialReferences.getWgs84())
+        val hike3loc = Point(-111.584847,43.71025798, SpatialReferences.getWgs84())
+        val hike4loc = Point(-111.629959, 43.6457475, SpatialReferences.getWgs84())
+        val hike5loc = Point(-111.33833, 43.77083, SpatialReferences.getWgs84())
+        val hike6loc = Point(-111.1217377, 43.4343979, SpatialReferences.getWgs84())
+        val hike7loc = Point(-111.1599426, 43.430475, SpatialReferences.getWgs84())
+        val hike8loc = Point(-113.9569876, 44.89805017, SpatialReferences.getWgs84())
+        val hike9loc = Point(-111.920776, 41.226144, SpatialReferences.getWgs84())
+        val hike10loc = Point(-111.4366406, 44.3293007, SpatialReferences.getWgs84())
+        val hike11loc = Point(-111.403623, 44.6415117, SpatialReferences.getWgs84())
+        val hike12loc = Point(-111.215, 43.39566667, SpatialReferences.getWgs84())
+        val hike13loc = Point(-116.2760125, 47.8029711, SpatialReferences.getWgs84())
+        val hike14loc = Point(-110.6555556,42.85, SpatialReferences.getWgs84())
+        val hike15loc = Point(-118.751289, 36.612564, SpatialReferences.getWgs84())
+        val hike16loc = Point(-116.1842915, 45.1426709, SpatialReferences.getWgs84())
+        val hike17loc = Point(-112.3952537, 42.7543585, SpatialReferences.getWgs84())
+        val hike18loc = Point(-111.2410588, 44.518256, SpatialReferences.getWgs84())
+        val hike19loc = Point(-114.479698, 42.59919, SpatialReferences.getWgs84())
+        val hike20loc = Point(-111.6241667, 43.61111111, SpatialReferences.getWgs84())
 
         val hikeMarker = SimpleMarkerSymbol(SimpleMarkerSymbol.Style.DIAMOND, Color.argb(255, 0, 0, 128), 10.0F)
 
@@ -80,30 +98,236 @@ class MainActivity : AppCompatActivity() {
         cc["Length"] = "1.3mi"
         cc["Elevation"] = "311ft"
 
+        val wd: MutableMap<String, Any> = HashMap()
+        wd["Name"] = "Webster Dam"
+        wd["Length"] = "2.7mi"
+        wd["Elevation"] = "587ft"
+
+        val kc: MutableMap<String, Any> = HashMap()
+        kc["Name"] = "Kelly Canyon"
+        kc["Length"] = "3.7mi"
+        kc["Elevation"] = "613ft"
+
+        val pl: MutableMap<String, Any> = HashMap()
+        pl["Name"] = "Packsaddle Lake"
+        pl["Length"] = "5.2mi"
+        pl["Elevation"] = "1072ft"
+
+        val up: MutableMap<String, Any> = HashMap()
+        up["Name"] = "Upper Palisades"
+        up["Length"] = "15mi"
+        up["Elevation"] = "1679ft"
+
+        val lp: MutableMap<String, Any> = HashMap()
+        lp["Name"] = "Lower Palisades"
+        lp["Length"] = "9.9mi"
+        lp["Elevation"] = "1095ft"
+
+        val gb: MutableMap<String, Any> = HashMap()
+        gb["Name"] = "Gold Bug"
+        gb["Length"] = "3.5mi"
+        gb["Elevation"] = "958ft"
+
+        val hv: MutableMap<String, Any> = HashMap()
+        hv["Name"] = "Hidden Valley"
+        hv["Length"] = "7.4mi"
+        hv["Elevation"] = "941ft"
+
+        val mf: MutableMap<String, Any> = HashMap()
+        mf["Name"] = "Mesa Falls"
+        mf["Length"] = "2mi"
+        mf["Elevation"] = "91ft"
+
+        val hl: MutableMap<String, Any> = HashMap()
+        hl["Name"] = "Henry's Lake"
+        hl["Length"] = "3.2mi"
+        hl["Elevation"] = "187ft"
+
+        val wc: MutableMap<String, Any> = HashMap()
+        wc["Name"] = "Waterfall Canyon"
+        wc["Length"] = "23.1mi"
+        wc["Elevation"] = "4143ft"
+
+        val ec: MutableMap<String, Any> = HashMap()
+        ec["Name"] = "Little Elk Creek"
+        ec["Length"] = "6.2mi"
+        ec["Elevation"] = "2608ft"
+
+        val ls: MutableMap<String, Any> = HashMap()
+        ls["Name"] = "Lower Sheep Creek Canyon"
+        ls["Length"] = "1.2mi"
+        ls["Elevation"] = "104ft"
+
+        val dcc: MutableMap<String, Any> = HashMap()
+        dcc["Name"] = "Deep Creek Crest"
+        dcc["Length"] = "5.6mi"
+        dcc["Elevation"] = "1597ft"
+
+        val ck: MutableMap<String, Any> = HashMap()
+        ck["Name"] = "Corral Creek"
+        ck["Length"] = "6.5mi"
+        ck["Elevation"] = "761ft"
+
+        val cs: MutableMap<String, Any> = HashMap()
+        cs["Name"] = "Cherry Springs"
+        cs["Length"] = "2mi"
+        cs["Elevation"] = "137ft"
+
+        val bc: MutableMap<String, Any> = HashMap()
+        bc["Name"] = "Black Canyon"
+        bc["Length"] = "12.6mi"
+        bc["Elevation"] = "2532ft"
+
+        val sf: MutableMap<String, Any> = HashMap()
+        sf["Name"] = "South Fork Snake"
+        sf["Length"] = "14.2mi"
+        sf["Elevation"] = "3497ft"
+
+        val ss: MutableMap<String, Any> = HashMap()
+        ss["Name"] = "Stinking Springs"
+        ss["Length"] = "4.6mi"
+        ss["Elevation"] = "793ft"
+
         return arrayOf(
-            Graphic(hike2loc, cc, hikeMarker), Graphic(hike1loc, rm, hikeMarker)
+            Graphic(hike2loc, cc, hikeMarker),
+            Graphic(hike1loc, rm, hikeMarker),
+            Graphic(hike3loc, wd, hikeMarker),
+            Graphic(hike4loc, kc, hikeMarker),
+            Graphic(hike5loc, pl, hikeMarker),
+            Graphic(hike6loc, up, hikeMarker),
+            Graphic(hike7loc, lp, hikeMarker),
+            Graphic(hike8loc, gb, hikeMarker),
+            Graphic(hike9loc, hv, hikeMarker),
+            Graphic(hike10loc, mf, hikeMarker),
+            Graphic(hike11loc, hl, hikeMarker),
+            Graphic(hike12loc, wc, hikeMarker),
+            Graphic(hike13loc, ec, hikeMarker),
+            Graphic(hike14loc, ls, hikeMarker),
+            Graphic(hike15loc, dcc, hikeMarker),
+            Graphic(hike16loc, ck, hikeMarker),
+            Graphic(hike17loc, cs, hikeMarker),
+            Graphic(hike18loc, bc, hikeMarker),
+            Graphic(hike19loc, sf, hikeMarker),
+            Graphic(hike20loc, ss, hikeMarker)
         )
     }
 
     private fun createTextGraphics(): Array<Graphic> {
         // create a point geometry
         val rMountLoc = Point(-111.9902, 43.785, SpatialReferences.getWgs84())
-        val ccMountloc = Point(-111.7178538, 43.65749903, SpatialReferences.getWgs84())
+        val ccMountLoc = Point(-111.7178538, 43.65749903, SpatialReferences.getWgs84())
+        val wdMountLoc = Point(-111.584847,43.71025798, SpatialReferences.getWgs84())
+        val kcMountLoc = Point(-111.629959, 43.6457475, SpatialReferences.getWgs84() )
+        val plMountLoc = Point(-111.33833, 43.77083, SpatialReferences.getWgs84())
+        val upMountLoc = Point(-111.1217377, 43.4343979, SpatialReferences.getWgs84())
+        val lpMountLoc = Point(-111.1599426, 43.430475, SpatialReferences.getWgs84())
+        val gbMountLoc = Point(-113.9569876, 44.89805017, SpatialReferences.getWgs84())
+        val hvMountLoc = Point(-111.920776, 41.226144, SpatialReferences.getWgs84())
+        val mfMountLoc = Point(-111.4366406, 44.3293007, SpatialReferences.getWgs84())
+        val hlMountLoc = Point(-111.403623, 44.6415117, SpatialReferences.getWgs84())
+        val wcMountLoc = Point(-111.215, 43.39566667, SpatialReferences.getWgs84())
+        val ecMountLoc = Point(-116.2760125, 47.8029711, SpatialReferences.getWgs84())
+        val lsMountLoc = Point(-110.6555556,42.85, SpatialReferences.getWgs84())
+        val dccMountLoc = Point(-118.751289, 36.612564, SpatialReferences.getWgs84())
+        val ckMountLoc = Point(-116.1842915, 45.1426709, SpatialReferences.getWgs84())
+        val csMountLoc = Point(-112.3952537, 42.7543585, SpatialReferences.getWgs84())
+        val bcMountLoc = Point(-111.2410588, 44.518256, SpatialReferences.getWgs84())
+        val sfMountLoc = Point(-114.479698, 42.59919, SpatialReferences.getWgs84())
+        val ssMountLoc = Point(-111.6241667, 43.61111111, SpatialReferences.getWgs84())
+
 
         // create text symbols
         val rMountSymbol = TextSymbol(
-            20.0F, "R Mountain" , Color.argb(255, 0, 0, 128),
+            15.0F, "R Mountain" , Color.argb(255, 0, 0, 128),
             TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
         val ccMountSymbol = TextSymbol(
-            20.0F, "Cress Creek" , Color.argb(255, 0, 0, 128),
+            15.0F, "Cress Creek" , Color.argb(255, 0, 0, 128),
             TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
         // define graphics from each geometry and symbol
-        val rMountGraphic = Graphic(rMountLoc, rMountSymbol)
-        val ccMountGraphic = Graphic(ccMountloc, ccMountSymbol)
+        val wdMountSymbol = TextSymbol(
+            15.0F, "Webster Dam" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val kcMountSymbol = TextSymbol(
+            15.0F, "Kelly Canyon" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val plMountSymbol = TextSymbol(
+            15.0F, "Packsaddle Lake" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val upMountSymbol = TextSymbol(
+            15.0F, "Upper Palisades" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val lpMountSymbol = TextSymbol(
+            15.0F, "Lower Palisades" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val gbMountSymbol = TextSymbol(
+            15.0F, "Gold Bug" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val hvMountSymbol = TextSymbol(
+            15.0F, "Hidden Valley" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val mfMountSymbol = TextSymbol(
+            15.0F, "Mesa Falls" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val hlMountSymbol = TextSymbol(
+            15.0F, "Henry's Lake" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val wcMountSymbol = TextSymbol(
+            15.0F, "Waterfall Canyon" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val ecMountSymbol = TextSymbol(
+            15.0F, "Little Elk Creek" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val lsMountSymbol = TextSymbol(
+            15.0F, "Lower Sheep Creek Canyon" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val dccMountSymbol = TextSymbol(
+            15.0F, "Deep Creek Crest" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val ckMountSymbol = TextSymbol(
+            15.0F, "Corral Creek" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val csMountSymbol = TextSymbol(
+            15.0F, "Cherry Springs" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val bcMountSymbol = TextSymbol(
+            15.0F, "Black Canyon" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val sfMountSymbol = TextSymbol(
+            15.0F, "South Fork Snake" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
+        val ssMountSymbol = TextSymbol(
+            15.0F, "Stinking Springs" , Color.argb(255, 0, 0, 128),
+            TextSymbol.HorizontalAlignment.LEFT, TextSymbol.VerticalAlignment.BOTTOM)
 
+
+
+        val rMountGraphic = Graphic(rMountLoc, rMountSymbol)
+        val ccMountGraphic = Graphic(ccMountLoc, ccMountSymbol)
+        val wdMountGraphic = Graphic(wdMountLoc, wdMountSymbol)
+        val kcMountGraphic = Graphic(kcMountLoc, kcMountSymbol)
+        val plMountGraphic = Graphic(plMountLoc, plMountSymbol)
+        val upMountGraphic = Graphic(upMountLoc, upMountSymbol)
+        val lpMountGraphic = Graphic(lpMountLoc, lpMountSymbol)
+        val gbMountGraphic = Graphic(gbMountLoc, gbMountSymbol)
+        val hvMountGraphic = Graphic(hvMountLoc, hvMountSymbol)
+        val mfMountGraphic = Graphic(mfMountLoc, mfMountSymbol)
+        val hlMountGraphic = Graphic(hlMountLoc, hlMountSymbol)
+        val wcMountGraphic = Graphic(wcMountLoc, wcMountSymbol)
+        val ecMountGraphic = Graphic(ecMountLoc, ecMountSymbol)
+        val lsMountGraphic = Graphic(lsMountLoc, lsMountSymbol)
+        val dccMountGraphic = Graphic(dccMountLoc, dccMountSymbol)
+        val ckMountGraphic = Graphic(ckMountLoc, ckMountSymbol)
+        val csMountGraphic = Graphic(csMountLoc, csMountSymbol)
+        val bcMountGraphic = Graphic(bcMountLoc, bcMountSymbol)
+        val sfMountGraphic = Graphic(sfMountLoc, sfMountSymbol)
+        val ssMountGraphic = Graphic(ssMountLoc, ssMountSymbol)
 
         //return array
-        return arrayOf(rMountGraphic, ccMountGraphic)
+        return arrayOf(rMountGraphic, ccMountGraphic, wdMountGraphic, kcMountGraphic
+            ,plMountGraphic, upMountGraphic, lpMountGraphic, gbMountGraphic, hvMountGraphic
+            ,mfMountGraphic, hlMountGraphic, wcMountGraphic, ecMountGraphic, lsMountGraphic
+            ,dccMountGraphic ,ckMountGraphic, csMountGraphic, bcMountGraphic, sfMountGraphic
+            ,ssMountGraphic)
 
     }
 
